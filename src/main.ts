22168327ba23673d29562e934f83ad197072e54d
HashashinMaster <mehdibouzkoura1@gmail.com>
2023-05-27 19:11:45 +0200

    added some things

--- src/main.ts
+++ src/main.ts
@@ -25,13 +25,54 @@ path
   .attr("fill", "red")
   .attr("stroke-width", "2px");
 
+/**
+ * Defining the Shape Object to store mouse position
+ */
+interface shapeValues {
+  cordinates: number;
+  line: string[];
+  destination: string;
+}
+const pathValues: shapeValues = {
+  cordinates: 0,
+  line: [],
+  destination: "",
+};
+
+/**
+ * adding mouse enter event to know from where
+ * the mouse entered the box
+ */
+path.on("mouseenter", function (e: MouseEvent) {
+  const { offsetY } = e;
+  // Cheking if the mouse enter from top of the box
+  if (offsetY <= 50 + 20) {
+    pathValues.cordinates = 40;
+    pathValues.destination = "top";
+    pathValues.line = ["0 30", "90 30", "90 0"];
+  }
+  // Cheking if the mouse enter from bottom of the box
+  if (offsetY <= 120 + 20 && offsetY > 50 + 20) {
+    pathValues.cordinates = 120;
+    pathValues.destination = "bottom";
+    pathValues.line = ["0 -30", "90 -30", "90 0"];
+  }
+});
+
+/**
+ * Adding mouse move event to change the curve coordinates
+ */
 path.on("mousemove", function (e: MouseEvent) {
-  const { clientX, clientY } = e;
+  const { offsetY, offsetX } = e;
+
+  if (offsetY <= 50 + 20) console.log("ja mn lfo9e");
+  if (offsetY <= 120 + 20 && offsetY > 50 + 20) console.log("ja mn lt7t");
   path
     .transition()
     .attr(
       "d",
-      `M 50 40, h 240 ,v 80, h-240, v-80 ,M ${clientX} ${clientY} c 0 30, 90 30, 90 0, M 50 120, c 0 30, 90 30, 90 0 `
-    )
-    .delay(500);
+      `M 50 40, h 240 ,v 80, h-240, v-80 ,M ${offsetX} ${
+        pathValues.cordinates
+      } c ${pathValues.line.join(", ")}`
+    );
 });
