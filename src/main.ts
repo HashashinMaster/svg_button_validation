a5028125b4b00bcd3e279374ca7206762e7b5abf
HashashinMaster <mehdibouzkoura1@gmail.com>
2023-05-29 20:03:34 +0200

    adding inputs and changing svg width

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as d3 from "d3";
 /**
  * Adding Svg element to the div#app
  */
-const width = 600;
+const width = 300;
 const height = 300;
 const svg = d3
   .select("#app")
@@ -22,19 +22,19 @@ const curvePathMirror = svg.append("path");
  * Creating a box path and
  */
 boxPath
-  .attr("d", "M 50 40, h 240 ,v 80, h-240, v-80  ")
+  .attr("d", `M 30 100, h 240 ,v 80, h-240, v-80`)
   .attr("fill", "red")
   .attr("stroke-width", "2px");
 /**
  * initializing brezier curves
  */
 curvePath
-  .attr("d", "c 0 0, 0 0, 0 0 ")
+  .attr("d", "c 0 0, 0 0, 0 0, 0 0 ")
   .attr("fill", "white")
   .attr("stroke-width", "2px")
   .attr("stroke", "white");
 curvePathMirror
-  .attr("d", "c 0 0, 0 0, 0 0 ")
+  .attr("d", "c 0 0, 0 0, 0 0, 0 0 ")
   .attr("fill", "red")
   .attr("stroke-width", "2px")
   .attr("stroke", "white");
@@ -59,39 +59,36 @@ const pathValues: shapeValues = {
 boxPath.on("mousemove", function (e: MouseEvent) {
   const { offsetY, offsetX } = e;
   // Cheking if the mouse enter from top of the box
-  if (offsetY <= 50 + 20) {
-    pathValues.cordinates = 40;
+  if (offsetY <= 100 + 20) {
+    pathValues.cordinates = 100;
     pathValues.destination = "top";
     pathValues.line = ["0 40", "90 40", "90 0"];
   }
-
-  // Cheking if the mouse enter from bottom of the box
-  if (offsetY <= 120 + 20 && offsetY > 50 + 20) {
-    pathValues.cordinates = 120;
+  // Cheking if the mouse enters from bottom of the box
+  console.log(offsetX);
+  if (offsetY <= 180 + 20 && offsetY > 100 + 20) {
+    pathValues.cordinates = 180;
     pathValues.destination = "bottom";
     pathValues.line = ["0 -40", "90 -40", "90 0"];
   }
-  // making curve grows when the mouse goes deeper in the top. Yamete ૮⸝⸝> ̫ <⸝⸝ ა
+  /**
+   * Making curve grows when the mouse goes deeper in the top. Yamete ૮⸝⸝> ̫ <⸝⸝ ა.
+   * Top formula is : (curve size) - ( (mouse position) - box  Y position ).
+   * Bottom formula is : (curve size) - ( (box - Y position) - (mouse position) ).
+   */
   if (pathValues.destination === "top") {
-    pathValues.line[0] = `0 ${offsetY}`;
-    pathValues.line[1] = `90 ${offsetY}`;
+    pathValues.line[0] = `0 ${40 + (offsetY - 100)}`; //
+    pathValues.line[1] = `90 ${40 + (offsetY - 100)}`;
   } else {
-    /**
-     * when he goes in the bottom i calculate how much it lost from
-     * it's originals size: original size ((originalSize - boxSize) - (mousePos - originalSize))
-     */
-    pathValues.line[0] = `0 -${
-      40 + (offsetY - 40 > 40 ? 80 - (offsetY - 40) : 40)
-    }`;
-    pathValues.line[1] = `90 -${
-      40 + (offsetY - 40 > 40 ? 80 - (offsetY - 40) : 40)
-    }`;
+    pathValues.line[0] = `0 -${40 + (180 - offsetY)}`;
+    pathValues.line[1] = `90 -${40 + (180 - offsetY)}`;
   }
 
   /**
    * checking the curves if they are overflowing from the edges
    */
-  if (offsetX <= 80) {
+  if (offsetX <= 70) {
+    console.log("i entered  ");
     curvePath.attr(
       "d",
       `M ${offsetX} ${pathValues.cordinates} c ${pathValues.line.join(", ")}`
@@ -99,38 +96,38 @@ boxPath.on("mousemove", function (e: MouseEvent) {
     curvePathMirror.attr(
       "d",
       `M ${offsetX} ${
-        pathValues.destination === "bottom" ? 40 : 120
+        pathValues.destination === "bottom" ? 100 : 180
       } c ${pathValues.line.join(", ")}`
     );
-  } else if (offsetX >= 242) {
+  } else if (offsetX >= 219) {
     curvePath.attr(
       "d",
-      `M ${241 - 40} ${pathValues.cordinates} c ${pathValues.line.join(", ")}`
+      `M ${219 - 40} ${pathValues.cordinates} c ${pathValues.line.join(", ")}`
     );
     curvePathMirror.attr(
       "d",
-      `M ${241 - 40} ${
-        pathValues.destination === "bottom" ? 40 : 120
+      `M ${219 - 40} ${
+        pathValues.destination === "bottom" ? 100 : 180
       } c ${pathValues.line.join(", ")}`
     );
   } else {
     curvePath.attr(
       "d",
-      `M ${offsetX - 30} ${pathValues.cordinates} c ${pathValues.line.join(
+      `M ${offsetX - 40} ${pathValues.cordinates} c ${pathValues.line.join(
         ", "
       )}`
     );
     curvePathMirror.attr(
       "d",
-      `M ${offsetX - 30} ${
-        pathValues.destination === "bottom" ? 40 : 120
+      `M ${offsetX - 40} ${
+        pathValues.destination === "bottom" ? 100 : 180
       } c ${pathValues.line.join(", ")}`
     );
   }
 });
 
 /**
- * Removing curves after the user leaves the box
+ * Removing the curves after the user leaves the box
  */
 curvePath.on("mouseleave", function (e: MouseEvent) {
   e.stopPropagation();
